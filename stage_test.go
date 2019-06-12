dacb239b5da75a6f3d33cc798e9bcc3d0e7cdaa6
wurui <rui1009479218@gmail.com>
2019-06-12 14:33:38 +0800

    add stage test

--- stage_test.go
+++ stage_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -11,6 +13,46 @@ func contain(list []float64, ele float64) bool {
 		}
 	}
 	return false
+}
+
+func byteSliceEqual(a, b []byte) bool {
+	if (a == nil) != (b == nil) {
+		return false
+	}
+
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func Test_newStage_draw(t *testing.T) {
+	width1, height1 := 3, 3
+	stage1 := newStage(width1, height1)
+
+	if !byteSliceEqual(stage1.matrix, []byte{124, 45, 124, 10, 124, 32, 124, 10, 124, 45, 124, 10}) ||
+		!reflect.DeepEqual(stage1.mapping, map[float64]int{4: 8, 7: 9, 0: 0, 1: 1, 3: 2, 1.5: 4, 3.5: 5, 6.5: 6, 11: 10}) {
+		t.Errorf("newStage(%d, %d) return mapping is %v, matrix is %v. not match except", width1, height1, stage1.mapping, stage1.matrix)
+	}
+
+	coords1 := []coordinate{
+		{x: 0, y: 0},
+		{x: 1, y: 2},
+		{x: 5, y: 5},
+	}
+
+	m1 := stage1.draw(coords1)
+	except1 := []byte{0, 45, 124, 10, 124, 32, 124, 10, 124, 0, 124, 10}
+	if !byteSliceEqual(m1, except1) {
+		t.Errorf("newStage(%d, %d).draw(%v) return %v except %v", width1, height1, coords1, m1, except1)
+	}
+	fmt.Println(m1)
 }
 
 func Test_cantorPairing(t *testing.T) {
