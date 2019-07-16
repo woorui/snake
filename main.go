bcfe20f08c9bbc775cc5d4831fbe11ff3da17a71
wurui <wurui@guanghe.tv>
2019-07-16 14:37:27 +0800

    adjust Throttle interval

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 	food := newFood(1, stage.width-1, 1, stage.height-1, snake.getCoords())
 
 	input := make(chan byte)
-	go keyPress(input, snake.speed)
+	go keyPress(input, snake.speed/2)
 	go snake.adapt(input)
 
 	render(screen, stage, snake, food)
