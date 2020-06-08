b308d814ffa7b13227da11df718fb00628d2025e
woorui <rui1009479218@gmail.com>
2020-06-08 16:31:49 +0800

    race

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ func main() {
 	for {
 		select {
 		case <-moving:
-			screenClear(screen)
+			screenClear(screen) // race here
 			if snake.checkCollidingSelf() || len(snake.body) >= (stage.width-2)*(stage.height-2) {
 				fmt.Println("Game over, Your score is ", len(snake.body)-1)
 				exit()
