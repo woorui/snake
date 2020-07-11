470bc17a35191e0c1d13ae804d905172cda3af89
woorui <rui1009479218@gmail.com>
2020-07-11 20:52:21 +0800

    update coord

---
+++ coord.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+)
+
+// Coord is coord
+type Coord struct {
+	ink byte
+	x   int
+	y   int
+}
+
+// CoordList is an array of Coord
+type CoordList []Coord
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// NewCoord return a new coord within NewCoord minX, maxX, minY, maxY.
+// If new coord within ban area, generate it again.
+func NewCoord(minX, maxX, minY, maxY int, ink byte, ban []Coord) Coord {
+	x := randRange(minX, maxX)
+	y := randRange(minY, maxY)
+
+	for _, v := range ban {
+		if v.x == x && v.y == y {
+			return NewCoord(minX, maxX, minY, maxY, ink, ban)
+		}
+	}
+
+	return Coord{x: x, y: y, ink: ink}
+}
+
+func (list CoordList) shift() Coord {
+	if len(list) == 0 {
+		return Coord{}
+	}
+	s := list[0]
+	list = list[1:]
+	return s
+}
+
+func (list CoordList) push(ele Coord) {
+	list = append(list, ele)
+}
+
+func (list CoordList) contain(ele Coord) bool {
+	if len(list) == 0 {
+		return false
+	}
+	for _, item := range list {
+		if ele.x == item.x && ele.y == item.y {
+			return true
+		}
+	}
+	return false
+}
