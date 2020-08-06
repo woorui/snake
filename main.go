dd726c281a8132f26557ce5e9d46a6ff0314964f
woorui <rui1009479218@gmail.com>
2020-08-06 11:08:00 +0800

    remove reduance code

--- main.go
+++ main.go
@@ -1,24 +1,7 @@
 package main
 
-import (
-	"time"
-)
-
 func main() {
 	game := NewGame(GameOpts{})
 
 	game.Run()
-}
-
-func broadcastTicker(d time.Duration) (chan bool, chan struct{}) {
-	directionLock := make(chan bool)
-	moving := make(chan struct{})
-	ticker := time.NewTicker(d)
-	go func() {
-		for range ticker.C {
-			directionLock <- true
-			moving <- struct{}{}
-		}
-	}()
-	return directionLock, moving
 }
